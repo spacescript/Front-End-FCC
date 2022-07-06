0e62346fcd3fb2efd1e705462186b1f588604c3b
spacescript <simonbengoehring@gmail.com>
2022-07-05 20:09:56 -0700

    Update App.js

--- React/quote-machine/src/App.js
+++ React/quote-machine/src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 
 function App() {
   const [quote, setQuote] = useState("It is never too late to be what you might have been.")
+  const [author, setAuthor] = useState("George Eliot");
+  const [randomInteger, setRandomInteger] = useState(0);
 
-  const author = "George Eliot"
+  const changeQuoteAndAuthor = () => {
+    setQuote("Dream big and dare to fail.")
+    setAuthor("Norman Vaughan")
+  }
+
+  // const randomInteger = (Math.floor(Math.random() * 3))
+
   return (
     <div className="App">
       <header className="App-header">
         <div id="quote-box" >
+          <p>Random Number: </p>
+          <button onClick={()=> setRandomInteger(Math.floor(Math.random() * 3))} >Random Number: {randomInteger}</button>
           <p>
             "{quote}"
           </p>
           <p>- {author}</p>
-          <button onClick={() => setQuote("Dream big and dare to fail.")} >Change Quote</button>
+          <button onClick={changeQuoteAndAuthor} >Change Quote</button>
         </div>
       </header>
     </div>

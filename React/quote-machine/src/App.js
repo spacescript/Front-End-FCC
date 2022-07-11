ed782dc52ed2c77fd89543d483409c482f316228
spacescript <simonbengoehring@gmail.com>
2022-07-11 12:41:57 -0700

    Update App.js

--- React/quote-machine/src/App.js
+++ React/quote-machine/src/App.js
@@ -1,33 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
-let quoteDataBaseURL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
 
 function App() {
-  const [quote, setQuote] = useState("It is never too late to be what you might have been.")
-  const [author, setAuthor] = useState("George Eliot");
-  const [randomInteger, setRandomInteger] = useState(0);
-  const [quotesArray, setQuotesArray] = useState(null);
 
-  const changeQuoteAndAuthor = () => {
-    setQuote("Dream big and dare to fail.")
-    setAuthor("Norman Vaughan")
-  }
-
-  const generateRandomNumber = () => {
-    let randomInteger = Math.floor(Math.random() * 3)
-    setRandomInteger(randomInteger)
-  }
-
-  const fetchQuotes = async (url) => {
-    const response = await fetch(url)
-    const parsedJSON = await response.json()
-    setQuotesArray(parsedJSON)
-  }
-
-  useEffect(() => {
-    fetchQuotes(quoteDataBaseURL)
-  }, [quoteDataBaseURL])
 
   // const randomInteger = (Math.floor(Math.random() * 3))
 
@@ -35,13 +12,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div id="quote-box" >
-          <h1>Random Number: </h1>
-          <button onClick={generateRandomNumber} >Random Number: {randomInteger}</button>
           <p>
-            "{quote}"
+            "I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do."
           </p>
-          <p>- {author}</p>
-          <button onClick={changeQuoteAndAuthor} >Change Quote</button>
+          <p>- Leonardo da Vinci</p>
+          <button>New Quote</button>
         </div>
       </header>
     </div>

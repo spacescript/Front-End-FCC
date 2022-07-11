f5165944d11a9165cc95bb64ab92e7a3232013d2
spacescript <simonbengoehring@gmail.com>
2022-07-11 15:48:57 -0700

    Update App.js

--- React/quote-machine/src/App.js
+++ React/quote-machine/src/App.js
@@ -4,22 +4,35 @@ import './App.css';
 
 
 function App() {
-  const [quote, setQuote] = useState("I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.")
+  const [quote, setQuote] = useState("I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.");
+  const [author, setAuthor] = useState("Leonardo da Vinci");
+  const [randomInteger, setRandomInteger] = useState(0);
+
+  const fullQuoteUpdate = () => {
+    setQuote("Whatever the mind of man can conceive and believe, it can achieve.")
+    setAuthor("Napoleon Hill")
+  }
+
+  const newNumber = () => {
+    setRandomInteger(Math.floor(Math.random() * 3));
+  }
 
   // let quote = "I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do."
-  let author = "Leonardo da Vinci"
+  // let author = "Leonardo da Vinci"
 
   // const randomInteger = (Math.floor(Math.random() * 3))
 
   return (
     <div className="App">
       <header className="App-header">
-        <div id="quote-box" >
+        <div id="quote-box">
+          <h1>Random Number: {randomInteger}</h1>
+          <button onClick={newNumber} >Randomize Number</button>
           <p>
             "{quote}"
           </p>
           <p>- {author}</p>
-          <button onClick={() => setQuote("Whatever the mind of man can conceive and believe, it can achieve.")} >New Quote</button>
+          <button onClick={fullQuoteUpdate} >New Quote</button>
         </div>
       </header>
     </div>

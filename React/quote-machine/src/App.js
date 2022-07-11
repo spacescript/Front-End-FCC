60f400b24b61cab9f3c333606e4e2226e43348ae
spacescript <simonbengoehring@gmail.com>
2022-07-11 13:03:36 -0700

    Update App.js

--- React/quote-machine/src/App.js
+++ React/quote-machine/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 
 function App() {
+  const [quote, setQuote] = useState("I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.")
 
+  // let quote = "I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do."
+  let author = "Leonardo da Vinci"
 
   // const randomInteger = (Math.floor(Math.random() * 3))
 
@@ -13,10 +16,10 @@ function App() {
       <header className="App-header">
         <div id="quote-box" >
           <p>
-            "I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do."
+            "{quote}"
           </p>
-          <p>- Leonardo da Vinci</p>
-          <button>New Quote</button>
+          <p>- {author}</p>
+          <button onClick={() => setQuote("Whatever the mind of man can conceive and believe, it can achieve.")} >New Quote</button>
         </div>
       </header>
     </div>
